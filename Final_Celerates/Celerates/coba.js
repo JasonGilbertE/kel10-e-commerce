816bc57f316f715c45add40c3d3b9f32facb4d45
JasonGilbertE <jasongilbertefendi@gmail.com>
2024-11-17 23:50:02 +0700

    label user

---
+++ Final_Celerates/Celerates/coba.js
@@ -0,0 +1,5 @@
+// Fungsi untuk mengganti gambar utama
+function changeImage(thumbnail) {
+  const mainImage = document.getElementById("mainImage");
+  mainImage.src = thumbnail.src; // Mengubah sumber gambar utama
+}
